9488b10c195f2f80e99376c70376c3a9150b843d
thaflavorsaver <dojohnson@ucdavis.edu>
2023-05-06 18:47:07 -0700

    commented out metadata on subdirectory layouts, left metadata on root layout

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 const NavBar = () => {
+  const router = useRouter();
+  const handleHomeRoute = () => {
+    router.push("/");
+  };
+
   return (
     <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">
+      <button
+        className="btn btn-ghost normal-case text-xl"
+        onClick={handleHomeRoute}
+      >
         Pocket<b>PR</b>
-      </a>
+      </button>
     </div>
   );
 };
