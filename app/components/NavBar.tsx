a0eb00ef9896e16d8903ec8b338d6aa57895ddf8
thaflavorsaver <dojohnson@ucdavis.edu>
2023-05-06 21:12:33 -0700

    changed navbar, added drop down menu to right and centered pocketpr

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import {
-  useUser,
-  UserButton,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-} from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import { SignIn } from "@clerk/clerk-react";
 
 const NavBar = () => {
@@ -15,16 +9,51 @@ const NavBar = () => {
   const handleHomeRoute = () => {
     router.push("/");
   };
+  const handleDashboardRoute = () => {
+    router.push("/Dashboard");
+  };
 
   return (
-    <div className="navbar bg-primary justify-between p-5">
-      <button
-        className="btn btn-ghost normal-case text-xl"
-        onClick={handleHomeRoute}
-      >
-        Pocket<b>PR</b>
-      </button>
-      <div>
+    <div className="navbar bg-primary">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary rounded-box w-52"
+          >
+            <li>
+              <button onClick={handleDashboardRoute}>Dashboard</button>
+            </li>
+            {/* <li><a>About</a></li>
+            <li><a>Contact</a></li> */}
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <button
+          className="btn btn-ghost normal-case text-xl"
+          onClick={handleHomeRoute}
+        >
+          Pocket<b>PR</b>
+        </button>
+      </div>
+      <div className="navbar-end pr-3">
         <SignedIn>
           <UserButton />
         </SignedIn>
