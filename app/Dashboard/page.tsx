22207002b9377dcc642d1b6e6a6f33c8c2f6b632
guitarkeegan <keegananglim@gmail.com>
2023-05-08 20:42:15 -0700

    refactor dashboard to server side

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-
-import { useUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 
 export default function Home() {
-  const { isLoaded, isSignedIn, user } = useUser();
+  const { userId } = auth();
 
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
+  console.log(userId);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary-content">
       <div className="p-5">
-        Hello, {user.firstName}. Welcome to <b>PocketPR</b>.
+        Hello, {userId}. Welcome to <b>PocketPR</b>.
       </div>
     </main>
   );
