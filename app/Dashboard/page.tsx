9acd62cacc180f7dcd34e2fb3b090308252efe72
guitarkeegan <keegananglim@gmail.com>
2023-05-08 22:26:49 -0700

    add googleapis youtube

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,14 +1,13 @@
-import { auth } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   const { userId } = auth();
-
-  console.log(userId);
+  const user = await currentUser();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary-content">
       <div className="p-5">
-        Hello, {userId}. Welcome to <b>PocketPR</b>.
+        Hello, {user?.firstName}. Welcome to <b>PocketPR</b>.
       </div>
     </main>
   );
