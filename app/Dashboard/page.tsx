98418ef9bde4edfb350918dd1c60479bebe16e6e
thaflavorsaver <dojohnson@ucdavis.edu>
2023-05-09 19:10:32 -0700

    fixed merge issue

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,21 +1,14 @@
-"use client";
+import { auth, currentUser } from "@clerk/nextjs";
 
-import { useUser } from "@clerk/nextjs";
-
-export default function Home() {
-  const { isLoaded, isSignedIn, user } = useUser();
-
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
+export default async function Home() {
+  const { userId } = auth();
+  const user = await currentUser();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary-content">
       <div className="p-5">
-
         Hello, {user?.firstName}. Welcome to <b>PocketPR</b>. Your user ID is{" "}
         {userId}.
-
       </div>
     </main>
   );
