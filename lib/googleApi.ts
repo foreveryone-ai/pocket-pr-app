fe0a20005c762b5c42eb94c631a34530e04d7985
guitarkeegan <keegananglim@gmail.com>
2023-05-12 14:43:18 -0700

    add call for latest videos

--- lib/googleApi.ts
+++ lib/googleApi.ts
@@ -1,3 +1,4 @@
+import { google } from "googleapis";
 async function getOAuthData(userId: string, provider: string) {
   const res = await fetch(
     `https://api.clerk.com/v1/users/${userId}/oauth_access_tokens/${provider}`,
@@ -11,4 +12,4 @@ async function getOAuthData(userId: string, provider: string) {
   return data;
 }
 
-export default getOAuthData;
+export { google, getOAuthData };
