82b7f00d1917cad52582ba3ae94f95a820793ca3
guitarkeegan <keegananglim@gmail.com>
2023-05-18 20:17:45 -0700

    refactoring dashboard

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { auth, currentUser } from "@clerk/nextjs";
 // TODO: refactor, move this to lib
 // get the OAuth token from clerk
-import { getOAuthData, google } from "@/lib/googleApi";
+import { getOAuthData, google, getCommentsFromVideo } from "@/lib/googleApi";
 
 export default async function Home() {
   // can probably remove user, but keep userId
@@ -40,40 +40,7 @@ export default async function Home() {
       type: ["video"],
       maxResults: 5,
     });
-
-    try {
-      const myVideoId = "foKcZsIfxYs";
-      const res = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${myVideoId}&key=${process.env.GOOGLE_API}`,
-        {
-          headers: {
-            Accept: "application/json",
-          },
-        }
-      );
-      const commentsOneVideo = await res.json();
-      if (commentsOneVideo) {
-        // TODO: FIX THIS
-        commentsOneVideo.items.map((item: any) =>
-          console.log(item.replies.comments)
-        );
-      }
-    } catch (error) {
-      console.error(error);
-    }
-
-    // try {
-    //   commentsOneVideo = await yt.commentThreads.list({
-    //     part: ["id", "snippet", "replies"],
-    //     videoId: "foKcZsIfxYs"
-    //   });
-    // } catch (error) {
-    //   console.error(error);
-    //   throw Error ("Error getting comments");
-    // };
   }
-
-  // console.log(commentsOneVideo);
   // make a list of all channelIds that were returned
   console.log(
     "all channel Ids: ",
--- lib/googleApi.ts
+++ lib/googleApi.ts
@@ -1,4 +1,5 @@
 import { google } from "googleapis";
+
 async function getOAuthData(userId: string, provider: string) {
   const res = await fetch(
     `https://api.clerk.com/v1/users/${userId}/oauth_access_tokens/${provider}`,
@@ -12,4 +13,27 @@ async function getOAuthData(userId: string, provider: string) {
   return data;
 }
 
-export { google, getOAuthData };
+async function getCommentsFromVideo(videoId: string) {
+  try {
+    const myVideoId = "foKcZsIfxYs";
+    const res = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${myVideoId}&key=${process.env.GOOGLE_API}`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    const commentsOneVideo = await res.json();
+    if (commentsOneVideo) {
+      // TODO: FIX THIS
+      commentsOneVideo.items.map((item: any) =>
+        console.log(item.replies.comments)
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { google, getOAuthData, getCommentsFromVideo };
