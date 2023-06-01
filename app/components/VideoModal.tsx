a60e9b5dd817c2267b27fd85a70d6c8373506ece
thaflavorsaver <dojohnson@ucdavis.edu>
2023-05-31 18:44:41 -0700

    changed modal text

--- app/components/VideoModal.tsx
+++ app/components/VideoModal.tsx
@@ -11,16 +11,14 @@ export function VideoModal({ buttonText }: VideoModalProps) {
       <input type="checkbox" id="my-modal-6" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
+          <h3 className="font-bold text-lg">Continue?</h3>
           <p className="py-4">
-            You&apos;ve been selected for a chance to get one year of
-            subscription to use Wikipedia for free!
+            By clicking "proceed", you grant PocketPR permission to analyze the
+            comments on this video.
           </p>
           <div className="modal-action">
             <label htmlFor="my-modal-6" className="btn">
-              Yay!
+              Proceed
             </label>
           </div>
         </div>
