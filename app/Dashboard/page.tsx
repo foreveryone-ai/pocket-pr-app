d3f6dd71563c576c1fae501e9149675397b10fc6
thaflavorsaver <dojohnson@ucdavis.edu>
2023-05-31 19:29:12 -0700

    commit

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -61,8 +61,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary-content">
       <div className="p-5">
-        Hello, {user?.firstName}. Welcome to <b>PocketPR</b>. Your user ID is{" "}
-        {userId}.
+        Hello, {user?.firstName}. Welcome to <b>PocketPR</b>.
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
         {videos
