0a60bf75368c24d02af0333f36343260da8f4840
thaflavorsaver <dojohnson@ucdavis.edu>
2023-07-06 15:13:12 -0700

    api/formSubmit/route.ts working

--- app/api/formSubmit/route.ts
+++ app/api/formSubmit/route.ts
@@ -1,25 +1,43 @@
-import axios from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(req: NextRequest) {
+  // Check if the request method is POST
   if (req.method === "POST") {
     try {
-      const result = await axios.post(
-        "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec",
-        req.body,
+      // Set the headers for the google scripts api
+      const headers = new Headers({
+        "Content-Type": "application/x-www-form-urlencoded",
+      });
+
+      // Get the request body as JSON
+      const body = await req.json();
+
+      // Convert the JSON data to URL parameters
+      const formData = new URLSearchParams(body).toString();
+
+      // Send the data to the google scripts api endpoint
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyuQkqQaSIBv6ilviaIJBLExF_26wdzzbpjzGDW2SZ7yjikItSP0DpP-nCaVVK9A3R8/exec",
         {
-          headers: {
-            "Content-Type": "application/json",
-          },
+          method: "POST",
+          headers,
+          body: formData,
         }
       );
-      return res.status(200).json(result.data);
+
+      // Return the response from the google scripts api
+      const data = await response.json();
+      return new NextResponse(JSON.stringify(data), {
+        status: response.status,
+      });
     } catch (error) {
-      return res.status(500).json({ message: "Error submitting form" });
+      // Return a 500 Internal Server Error response
+      return new NextResponse(
+        JSON.stringify({ message: "Error submitting form", error }),
+        { status: 500 }
+      );
     }
   } else {
-    return res.status(405).json({ message: "Method not allowed" });
+    // Return a 405 Method Not Allowed response for other methods
+    return new NextResponse(null, { status: 405 });
   }
-};
-
-export default handler;
+}
