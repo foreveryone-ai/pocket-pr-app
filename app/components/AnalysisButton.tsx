e235dc07d365e6f597c132c37797780bbd816abd
guitarkeegan <keegananglim@gmail.com>
2023-08-10 23:54:35 -0700

    needed to cd out of nested directory

--- app/components/AnalysisButton.tsx
+++ app/components/AnalysisButton.tsx
@@ -1,11 +1,18 @@
 "use client";
+
+import { BaseSyntheticEvent } from "react";
+
 type AnalysisButtonProps = {
   title: string;
 };
 export default function AnalysisButton({ title }: AnalysisButtonProps) {
+  const handleClick = (event: BaseSyntheticEvent) => {
+    console.log(title);
+  };
+
   return (
     <div className="md:w-full w-[280px] collapse bg-[#7AD9F8] opacity-80 text-black text-center">
-      <input type="checkbox" className="w-full" />
+      <input onClick={handleClick} type="checkbox" className="w-full" />
       <div className="collapse-title text-xl font-medium">{title}</div>
       <div className="collapse-content ">
         <p className="text-black">description</p>
