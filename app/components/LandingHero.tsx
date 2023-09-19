0344f574f0cebffe39da47728ef43b16b60a5e63
thaflavorsaver <dojohnson@ucdavis.edu>
2023-09-19 00:05:57 -0700

    fixed TS errors on video embed

--- app/components/LandingHero.tsx
+++ app/components/LandingHero.tsx
@@ -135,9 +135,7 @@ export default function Hero() {
           height="315"
           src="https://www.youtube.com/embed/lbcpORKF-Gs?si=BEuhZ4_z6QBpKgFd&amp;controls=0"
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
         ></iframe>
       </div>
       <div className="flex flex-col justify-center items-center md:hidden ">
@@ -147,9 +145,7 @@ export default function Hero() {
           height="315"
           src="https://www.youtube.com/embed/lbcpORKF-Gs?si=BEuhZ4_z6QBpKgFd&amp;controls=0"
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
         ></iframe>
       </div>
 
