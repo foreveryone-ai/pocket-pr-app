f94fb17f727a32c282106a9c1a36261a5b58269b
thaflavorsaver <dojohnson@ucdavis.edu>
2023-09-23 19:33:12 -0700

    agree button conditionally rendered to first step

--- app/components/OnboardingPage.tsx
+++ app/components/OnboardingPage.tsx
@@ -113,59 +113,78 @@ export default function App() {
                   {step === 1 && (
                     <div style={{ overflowY: "auto", maxHeight: "200px" }}>
                       <p>
-                        1. Introduction Welcome to PocketPR! This User Agreement
-                        ("Agreement") governs your use of the PocketPR
-                        application ("App"). By accessing or using the App, you
-                        agree to be bound by the terms of this Agreement. If you
-                        do not agree with these terms, please do not use the
-                        App. 2. User Conduct You agree not to use the App to: a)
-                        Engage in or promote any illegal activities. b) Transmit
-                        or post any content that is violent, offensive, racist,
-                        discriminatory, hateful, or otherwise objectionable. c)
-                        Harass, threaten, or defame any person or entity. d)
-                        Transmit or post any content that infringes upon the
+                        <b>1. Introduction Welcome to PocketPR!</b>
+                        <br /> This User Agreement ("Agreement") governs your
+                        use of the PocketPR application ("App"). By accessing or
+                        using the App, you agree to be bound by the terms of
+                        this Agreement. If you do not agree with these terms,
+                        please do not use the App. <br />
+                        <b>2. User Conduct</b>
+                        <br /> You agree not to use the App to:
+                        <br />
+                        a) Engage in or promote any illegal activities. <br />
+                        b) Transmit or post any content that is violent,
+                        offensive, racist, discriminatory, hateful, or otherwise
+                        objectionable. <br />
+                        c) Harass, threaten, or defame any person or entity.{" "}
+                        <br />
+                        d) Transmit or post any content that infringes upon the
                         rights of others, including intellectual property
-                        rights. e) Engage in any activity that disrupts or
-                        interferes with the proper working of the App. 3. Chat
-                        Messages a) All chat messages exchanged with the App's
-                        chatbot are saved by PocketPR. b) Saved chat messages
-                        are used for the purpose of improving and training
-                        future models. c) All saved chat messages are anonymized
-                        to protect user privacy. Personal identifiers are
-                        removed to ensure that individual users cannot be
-                        identified from the saved data. 4. Privacy Your privacy
-                        is important to us. Please review our Privacy Policy,
-                        which is incorporated into this Agreement by reference,
-                        to understand how we collect, use, and disclose
-                        information. 5. Termination PocketPR reserves the right
-                        to terminate or suspend your access to the App without
-                        prior notice if you violate any terms of this Agreement
-                        or for any other reason at our sole discretion. 6.
-                        Disclaimers The App is provided "as is" and without
-                        warranties of any kind, either express or implied.
-                        PocketPR disclaims all warranties, express or implied,
-                        including, but not limited to, implied warranties of
-                        merchantability, fitness for a particular purpose, and
-                        non-infringement. 7. Limitation of Liability To the
-                        fullest extent permitted by applicable law, PocketPR
-                        shall not be liable for any indirect, incidental,
-                        special, consequential, or punitive damages, or any loss
-                        of profits or revenues, whether incurred directly or
-                        indirectly, or any loss of data, use, goodwill, or other
-                        intangible losses, resulting from your use or inability
-                        to use the App. 8. Changes to Agreement PocketPR
-                        reserves the right to modify this Agreement at any time.
-                        Changes will be effective immediately upon posting. Your
-                        continued use of the App after changes are posted
-                        constitutes your acceptance of the revised Agreement. 9.
-                        Governing Law This Agreement is governed by the laws of
-                        the jurisdiction in which PocketPR is based, without
-                        regard to its conflict of law provisions. 10. Contact
+                        rights.
+                        <br /> e) Engage in any activity that disrupts or
+                        interferes with the proper working of the App. <br />
+                        <b>3. Chat Messages</b> <br />
+                        a) All chat messages exchanged with the App's chatbot
+                        are saved by PocketPR. <br />
+                        b) Saved chat messages are used for the purpose of
+                        improving and training future models. <br />
+                        c) All saved chat messages are anonymized to protect
+                        user privacy. Personal identifiers are removed to ensure
+                        that individual users cannot be identified from the
+                        saved data.
+                        <br /> <b> 4. Privacy </b>
+                        <br /> Your privacy is important to us. Please review
+                        our Privacy Policy, which is incorporated into this
+                        Agreement by reference, to understand how we collect,
+                        use, and disclose information.
+                        <br /> <b> 5. Termination </b> <br />
+                        PocketPR reserves the right to terminate or suspend your
+                        access to the App without prior notice if you violate
+                        any terms of this Agreement or for any other reason at
+                        our sole discretion.
+                        <br /> <b> 6. Disclaimers </b>
+                        <br />
+                        The App is provided "as is" and without warranties of
+                        any kind, either express or implied. PocketPR disclaims
+                        all warranties, express or implied, including, but not
+                        limited to, implied warranties of merchantability,
+                        fitness for a particular purpose, and non-infringement.{" "}
+                        <br /> <b>7. Limitation of Liability </b>
+                        <br /> To the fullest extent permitted by applicable
+                        law, PocketPR shall not be liable for any indirect,
+                        incidental, special, consequential, or punitive damages,
+                        or any loss of profits or revenues, whether incurred
+                        directly or indirectly, or any loss of data, use,
+                        goodwill, or other intangible losses, resulting from
+                        your use or inability to use the App. <br />
+                        <b>8. Changes to Agreement </b>
+                        <br />
+                        PocketPR reserves the right to modify this Agreement at
+                        any time. Changes will be effective immediately upon
+                        posting. Your continued use of the App after changes are
+                        posted constitutes your acceptance of the revised
+                        Agreement. <br /> <b>9. Governing Law </b>
+                        <br /> This Agreement is governed by the laws of the
+                        jurisdiction in which PocketPR is based, without regard
+                        to its conflict of law provisions.
+                        <br />
+                        <b>10. Contact</b>
+                        <br />
                         For any questions or concerns regarding this Agreement,
-                        please contact us at [contact@pocketpr.com]. By using
-                        the PocketPR App, you acknowledge that you have read,
-                        understood, and agree to be bound by the terms of this
-                        User Agreement.
+                        please contact us at <u>help@pocketpr.com</u>. <br />
+                        By using the PocketPR App, you acknowledge that you have
+                        read, understood, and agree to be bound by the terms of
+                        this User Agreement.
                       </p>
                     </div>
                   )}
@@ -192,28 +211,13 @@ export default function App() {
                   <Button color="danger" variant="light" onPress={onClose}>
                     Cancel
                   </Button>
-
-                  {step < 4 ? (
-                    <Button
-                      color="primary"
-                      onPress={nextStep}
-                      isLoading={isLoading}
-                    >
-                      Next
-                    </Button>
-                  ) : (
-                    <Button
-                      color="primary"
-                      onPress={() => {
-                        console.log("finished button..");
-                        onClose();
-                        nextStep();
-                      }}
-                      isLoading={isLoading}
-                    >
-                      Let&apos;s do it!
-                    </Button>
-                  )}
+                  <Button
+                    color="primary"
+                    onPress={nextStep}
+                    isLoading={isLoading}
+                  >
+                    {step === 1 ? "Agree" : step < 4 ? "Next" : "Let's do it!"}
+                  </Button>
                 </ModalFooter>
               </>
             )}
