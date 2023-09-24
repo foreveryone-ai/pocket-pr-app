27546b0a9186bfd544835b33c384c193aa39cbd9
thaflavorsaver <dojohnson@ucdavis.edu>
2023-09-23 18:18:32 -0700

    added spinner to update button that resolves upon completion of call

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -5,26 +5,33 @@ import { BiLogoDiscord } from "react-icons/bi";
 import { AiFillInstagram } from "react-icons/ai";
 import { Button } from "@nextui-org/button";
 import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/navbar";
+import { useState } from "react";
+import { Spinner } from "@nextui-org/spinner";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/update-youtube", {
+        method: "GET",
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <Navbar>
         <NavbarBrand>
-          <Button
-            onClick={async () => {
-              try {
-                const response = await fetch("/api/update-youtube", {
-                  method: "GET",
-                });
-                const data = await response.json();
-                console.log(data);
-              } catch (error) {
-                console.error(error);
-              }
-            }}
-          >
-            Update
+          <Button onClick={handleClick}>
+            {isLoading ? <Spinner /> : "Update"}
           </Button>
         </NavbarBrand>
         <NavbarContent className="" justify="center">
