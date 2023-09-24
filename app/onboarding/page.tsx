f06b1cbc771bc8fccb3d9ef97c8599c7d7d2c112
thaflavorsaver <dojohnson@ucdavis.edu>
2023-09-24 14:08:20 -0700

    got rid of scroll on onboarding page

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,6 +3,20 @@ import { auth, currentUser } from "@clerk/nextjs";
 import { createUser } from "@/lib/supabaseClient";
 import OnboardingPage from "../components/OnboardingPage";
 import NavBar from "@/app/components/NavBar";
+import { Playfair_Display } from "next/font/google";
+
+const playFairDisplay500 = Playfair_Display({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+const playFairDisplay650 = Playfair_Display({
+  weight: ["600"],
+  subsets: ["latin"],
+});
+const playFairDisplay800 = Playfair_Display({
+  weight: ["900"],
+  subsets: ["latin"],
+});
 
 export default async function Onboarding() {
   const { userId, getToken } = auth();
@@ -33,6 +47,14 @@ export default async function Onboarding() {
     <>
       <div className="min-h-screen bg-green-800">
         <NavBar />
+        <div className="flex flex-col py-48 justify-center items-center">
+          <h2 className={`py-4  text-3xl ${playFairDisplay800.className}`}>
+            Welcome! It&apos;s time to get you onboarded.
+          </h2>
+          <h1 className={`pb-12 text-2xl ${playFairDisplay500.className}`}>
+            In just a few clicks, we&apos;ll have you automated in no-time.
+          </h1>
+        </div>
         <OnboardingPage />
       </div>
     </>
