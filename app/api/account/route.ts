0e801877060d45b4841fc7cbcc5242a36462f130
guitarkeegan <keegananglim@gmail.com>
2023-09-27 12:48:07 -0700

    add stripe dashboard route

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -1,1 +1,28 @@
-export async function POST() {}
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  //check for stripe account in DB?
+  // or
+  // create new user in stripe?
+
+  const configuration = await stripe.billingPortal.configurations.create({
+    business_profile: {
+      headline: "ForEveryone.ai partners with Stripe for simplified billing.",
+    },
+    features: {
+      invoice_history: {
+        enabled: true,
+      },
+    },
+  });
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: "cus_OhmsYR5io1xIgS", // test customer
+    return_url: process.env.LOCAL_DOMAIN + "Dashboard",
+  });
+
+  if (session) {
+    return NextResponse.json({ url: session.url });
+  }
+}
