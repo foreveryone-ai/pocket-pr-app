6de91fd05d0a2059983f8bee0f19bb0f7777ffec
Daniel O'Connor Johnson <100829940+oconnorjohnson@users.noreply.github.com>
2023-10-04 09:35:20 -0700

    Update middleware.ts
    added "/privacy" to list of public routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 import { NextMiddleware, NextRequest } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/help", "/sign-in", "/sign-up"],
+  publicRoutes: ["/", "/help", "/sign-in", "/sign-up", "/privacy"],
 });
 
 export const config = {
