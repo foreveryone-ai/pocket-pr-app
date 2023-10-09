16c79229a4f2ca146cfc9615ef3ad431151cb836
thaflavorsaver <dojohnson@ucdavis.edu>
2023-10-09 16:39:13 -0700

    added channel_id to the update-youtube route

--- app/api/update-youtube/route.ts
+++ app/api/update-youtube/route.ts
@@ -117,7 +117,12 @@ export async function GET() {
     console.log(`getting all captions for ${videosToStore.length} videos`);
     for (let video of videosToStore) {
       try {
-        await GoogleApi.getCaptions(token, video.id, userOAuth[0].token);
+        await GoogleApi.getCaptions(
+          token,
+          video.id,
+          userOAuth[0].token,
+          video.channel_id
+        );
       } catch (error) {
         console.error(error);
       }
