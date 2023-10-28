f774bb71a0193b4e5a9dd621ceb90d6f41a9a72d
thaflavorsaver <dojohnson@ucdavis.edu>
2023-10-27 19:03:32 -0700

    onboarding backend refactored to /onboarding/agreement and /onboarding/retrieval and base of /upgrading started

--- app/upgrading/page.tsx
+++ app/upgrading/page.tsx
@@ -1,1 +1,32 @@
-export default function upgrading() {}
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@nextui-org/button";
+
+export default function upgrading() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchUpgradeData = async () => {
+    setIsLoading(true);
+    try {
+      // api calls go here
+    } catch (error) {
+      console.error(
+        "An error occured while fetching the user's upgrade data",
+        error
+      );
+      // if any API call fails, redirect to "/Dashboard"
+      router.push("/Dashboard");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Button isDisabled={isLoading} onPress={fetchUpgradeData}>
+        {isLoading ? "Loading..." : "Start Upgrade"}
+      </Button>
+    </>
+  );
+}
