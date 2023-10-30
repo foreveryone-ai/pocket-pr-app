e837dd32f274a6115773e6660ab9b2e596be6bb4
thaflavorsaver <dojohnson@ucdavis.edu>
2023-10-29 18:24:35 -0700

    added ProTabs component, removed channel-chat from Tabs component, added conditional rendering to Dashboard to render either Tabs or ProTabs depending on the user's subscription status

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,12 +1,18 @@
-import { getChannelId, getVideos, storeUserToken } from "@/lib/supabaseClient";
+// app/Dashboard/page.tsx
+import {
+  getChannelId,
+  getVideos,
+  storeUserToken,
+  getUserSubscriptionStatus,
+} from "@/lib/supabaseClient";
 import VideoCard from "@/app/components/VideoCards";
 import { auth, currentUser } from "@clerk/nextjs";
 import { Button } from "@nextui-org/button";
 import Tabs from "@/app/components/Tabs";
+import ProTabs from "@/app/components/ProTabs";
 import NavBar from "@/app/components/NavBar";
 import DesktopTrends from "@/app/components/DesktopTrends";
 
-// get the OAuth token from clerk
 export default async function Home() {
   const { userId, getToken } = auth();
   const token = await getToken({ template: "supabase" });
@@ -21,7 +27,6 @@ export default async function Home() {
   try {
     const user = await getChannelId(token as string, userId as string);
     youtube_channel_id = user?.data && user.data[0].youtube_channel_id;
-    //console.log("ch id: ", youtube_channel_id);
   } catch (error) {
     console.error("error on get channel id.. ", error);
   }
@@ -35,16 +40,20 @@ export default async function Home() {
     console.error(error);
   }
 
+  // Get the user's subscription status
+  const subscriptionStatus = await getUserSubscriptionStatus(userId as string);
+
   return (
     <div className="min-h-screen bg-white">
       <NavBar />
       <div className="pt-6">
-        <Tabs channelId={youtube_channel_id} />
+        {subscriptionStatus ? (
+          <ProTabs channelId={youtube_channel_id} />
+        ) : (
+          <Tabs channelId={youtube_channel_id} />
+        )}
         <div className="flex flex-col-1 items-center lg:flex-col-2 lg:items-start text-black justify-center pt-6 pb-6 bg-white ">
-          <div
-            className="border rounded-xl bordered bg-gray-50  pt-5 lg:pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 px-4 md:px-8 lg:px-10 overflow-y-auto h-screen-65 lg:h-screen-75"
-            // style={{ height: "750px" }}
-          >
+          <div className="border rounded-xl bordered bg-gray-50  pt-5 lg:pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 px-4 md:px-8 lg:px-10 overflow-y-auto h-screen-65 lg:h-screen-75">
             {videos
               ? videos.data?.map((video, i) => (
                   <VideoCard
@@ -53,16 +62,10 @@ export default async function Home() {
                     title={video.title as string}
                     imageUrl={video.thumbnail_url as string}
                     hasEmbeddings={video.hasEmbeddings}
-                    //TODO: store this in database
                   />
                 ))
               : "no videos found"}
           </div>
-
-          {/* <div className="hidden lg:block lg:px-4" /> */}
-
-          {/* --------------------Visible on LARGE screens and above-------------------- */}
-          {/* <DesktopTrends /> */}
         </div>
       </div>
     </div>
