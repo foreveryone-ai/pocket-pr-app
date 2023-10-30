ac8bda53e5b0d26c6b3d2b8c8e60e489b55bab81
thaflavorsaver <dojohnson@ucdavis.edu>
2023-10-30 15:49:40 -0700

    added base outline for @/app/components/landing/Hero.tsx

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -1,3 +1,53 @@
+import Image from "next/image";
 export default function Hero() {
-  return <>Hello from Hero</>;
+  return (
+    <>
+      <section className="bg-black">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
+              Silence the noise; <br />
+              Turn feedback to fame.
+            </h1>
+            <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">
+              From checkout to global sales tax compliance, companies around the
+              world use Flowbite to simplify their payment stack.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900"
+            >
+              Get started
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg  focus:ring-4 text-white border-gray-700 hover:bg-gray-700 focus:ring-gray-800"
+            >
+              Speak to Sales
+            </a>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <Image
+              src="/gunmetal-panda-asset.svg"
+              alt="panga"
+              height="400"
+              width="400"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
