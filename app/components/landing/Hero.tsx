5a3cb147f5e727bb873031686dd0c93caaa9e2d8
thaflavorsaver <dojohnson@ucdavis.edu>
2023-10-30 19:23:34 -0700

    components/landing/Hero.tsx done

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Hero() {
   return (
     <>
       <section className="bg-black">
         <div className="grid max-w-screen-xl px-8 xl:pl-36 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-11">
           <div className="place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
+            <h1 className="max-w-3xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
               Silence the noise; <br />
-              Turn <a className="gradient-text-feedback">feedback</a> to{" "}
+              Turn <a className="gradient-text-feedback">feedback</a> into{" "}
               <a className="gradient-text-fame">fame</a>.
             </h1>
             <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+              A happy audience is a happy life. We're building automated public
+              relations agents for content creators, influencers, and businesses
+              alike.
             </p>
-            <a
-              href="#"
+            <Link
+              href="/sign-up"
               className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900"
             >
               Get started
@@ -31,13 +33,7 @@ export default function Hero() {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-            </a>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg  focus:ring-4 text-white border-gray-700 hover:bg-gray-700 focus:ring-gray-800"
-            >
-              Speak to Sales
-            </a>
+            </Link>
           </div>
           <div className="hidden lg:col-span-3 lg:flex xl:pl-8 xl:pr-6">
             <Image
