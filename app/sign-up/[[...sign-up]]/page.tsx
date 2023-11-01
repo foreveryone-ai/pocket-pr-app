a6f8bdd2f36445a9311cd38c6926befadb59ed8a
thaflavorsaver <dojohnson@ucdavis.edu>
2023-11-01 14:45:50 -0700

    changed bg-green-800 to bg-black on /sign-in and /sign-up routes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "@/app/components/NavBar";
 export default function Page() {
   return (
     <>
-      <div className="flex flex-col items-center min-h-screen bg-green-800 justify-center w-full flex-1 px-20 text-center">
+      <div className="flex flex-col items-center min-h-screen bg-black justify-center w-full flex-1 px-20 text-center">
         <SignUp path="/sign-up" routing="path" />
       </div>
     </>
